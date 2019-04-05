67a4756d669531887e59a4cfac38cec5fa466751
zarden <y1352366024@yeah.net>
2019-04-05 11:38:18 +0800

    链式访问

---
+++ lianshifangwen/lianshifangwen/test.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+	char arr[20] = "hello";
+	int ret = strlen(strcat(arr, "bit"));
+	printf("%d\n ", ret);
+	system("pause");
+	return 0;
+}

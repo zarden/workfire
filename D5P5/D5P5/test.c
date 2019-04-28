21c3b3e37515003bdfe446117a073fdb69867af0
zarden <y1352366024@yeah.net>
2019-04-28 22:48:22 +0800

    0428

---
+++ D5P5/D5P5/test.c
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	int n;
+	int sn;
+	int i;
+	int tmp, sum = 0;
+	scanf("%d %d", &n, &sn);
+
+	for (i = n; i; i /= sn)
+	{
+		tmp = i % sn;
+		sum = sum * sn + tmp;
+	}
+	if (n == sum)
+	{
+		printf("%d在%d进制下是回文数\n", n, sn);
+	}
+	else
+	{
+		printf("%d在%d进制下不是回文数\n", n, sn);
+	}
+
+	system("pause");
+	return 0;
+}

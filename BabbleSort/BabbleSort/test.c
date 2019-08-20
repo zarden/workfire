47f0d9416fbb1c199059119f0d5e60e8abfc530b
zarden <y1352366024@yeah.net>
2019-08-20 23:25:17 +0800

    BabbleSort

---
+++ BabbleSort/BabbleSort/test.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void BabbleSort(int *a, int n)
+{
+	int tmp;
+	int i, j;
+
+	for (i=0 ; i < n-1; i++)
+	{
+		for (j = 0; j < n - i - 1; j++)
+		{
+			if (a[j]>a[j+1])
+			{
+				tmp = a[j];
+				a[j] = a[j + 1];
+				a[j + 1] = tmp;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int i;
+	int a[10] = { 5, 3, 6, 7, 9, 1, 2, 8, 4, 0 };
+	int n = sizeof(a) / sizeof(a[0]);
+	BabbleSort(a, n);
+	for ( i = 0; i < n; i++)
+	{
+		printf("%d\n", a[i]);
+	}
+	putchar('\n');
+	system("pause");
+	return 0;
+}
